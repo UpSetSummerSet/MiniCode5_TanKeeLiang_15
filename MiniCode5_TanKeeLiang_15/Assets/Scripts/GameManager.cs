f87ae00d4e662cbefcc65d618bb15d888cfa7881
UpSetSummerSet <72717739+UpSetSummerSet@users.noreply.github.com>
2020-12-02 15:31:01 +0800

    Done

--- MiniCode5_TanKeeLiang_15/Assets/Scripts/GameManager.cs
+++ MiniCode5_TanKeeLiang_15/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,9 +9,15 @@ public class GameManager : MonoBehaviour
 
     public GameObject addEnergyPrefab;
     public GameObject minusEnergyPrefab;
+    public GameObject TimerCounter;
+    public GameObject PlayerCon;
 
     public int numberOfSpawn;
     public float levelTime;
+    int AECollected = 1;
+    int ESpawn = 4;
+
+    int NumberOfSpawn = 50;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +27,11 @@ public class GameManager : MonoBehaviour
         }
         //Vector3 posToSpawn = new Vector3(Random.RandomRange(-10.0f, 10.0f), 0, Random.RandomRange(-10.0f, 10.0f));
         //Instantiate(addEnergyPrefab, posToSpawn, Quaternion.identity);
-        if(levelTime > 0)
+        if (levelTime > 0)
         {
             for (int i = 0; i < numberOfSpawn; i++)
             {
-                Vector3 posToSpawn = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
+                Vector3 posToSpawn = new Vector3(Random.Range(-25.0f, 25.0f), 0, Random.Range(-25.0f, 25.0f));
 
                 if (Random.Range(0, 2) < 1)
                 {
@@ -41,17 +48,21 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(levelTime > 0)
+        if (levelTime > 0)
         {
             levelTime -= Time.deltaTime;
             //print("levelTime: " + levelTime.ToString("0"));
-            print("levelTime: " + FormatTime(levelTime));
+            //print("levelTime: " + FormatTime(levelTime));
+
+            TimerCounter.GetComponent<Text>().text = "Time Left: " + FormatTime(levelTime);
         }
         else
         {
             levelTime = 0;
-            print("Times up!");
+            //print("Times up!");
+            TimerCounter.GetComponent<Text>().text = "Times up!";
         }
+        AddTime();
     }
 
     public string FormatTime(float time)
@@ -60,5 +71,29 @@ public class GameManager : MonoBehaviour
         int seconds = (int)time - 60 * minutes;
         int milliseconds = (int)(1000 * (time - minutes * 60 - seconds));
         return string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliseconds);
+    }
+
+    public void AddTime()
+    {
+        if(PlayerCon.GetComponent<PlayerController_15>().AECollected <= 0)
+        {
+            for(int i = 0; i < AECollected; i++)
+            {
+                for (i = 0; i < ESpawn; i++)
+                {
+                    Vector3 posToSpawn = new Vector3(Random.Range(-25.0f, 25.0f), 0, Random.Range(-25.0f, 25.0f));
+
+                    if (Random.Range(0, 2) < 1)
+                    {
+                        Instantiate(addEnergyPrefab, posToSpawn, Quaternion.identity);
+                    }
+                    else
+                    {
+                        Instantiate(minusEnergyPrefab, posToSpawn, Quaternion.identity);
+                    }
+                }
+                PlayerCon.GetComponent<PlayerController_15>().AECollected++;
+            }
+        }
     }
 }
---
+++ MiniCode5_TanKeeLiang_15/Assets/Scripts/PlayerController_15.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController_15 : MonoBehaviour
+{
+    float speed = 10.0f;
+    float energyCount = 0;
+    public int AECollected = 1;
+
+    public Animator playerAnim;
+    public GameObject ECounter;
+    public GameObject GameManager1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ECounter.GetComponent<Text>().text = "Energy Counter: " + energyCount;
+
+        if(GameManager1.GetComponent<GameManager>().levelTime > 0)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                playerAnim.SetBool("IsRunning", true);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+                playerAnim.SetBool("IsRunning", true);
+            }
+            else if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+            {
+                playerAnim.SetBool("IsRunning", false);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                transform.rotation = Quaternion.Euler(0, -90, 0);
+                playerAnim.SetBool("IsRunning", true);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                transform.rotation = Quaternion.Euler(0, 90, 0);
+                playerAnim.SetBool("IsRunning", true);
+            }
+            else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+            {
+                playerAnim.SetBool("IsRunning", false);
+            }
+        }
+        else if(GameManager1.GetComponent<GameManager>().levelTime == 0)
+        {
+            playerAnim.SetBool("IsRunning", false);
+        }
+
+        if (energyCount == 50)
+        {
+            SceneManager.LoadScene("WinScene");
+        }
+        else if (GameManager1.GetComponent<GameManager>().levelTime == 0 || energyCount < 0)
+        {
+            SceneManager.LoadScene("LoseScene");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("AddEnergy"))
+        {
+            energyCount += 5;
+            Destroy(collision.gameObject);
+            AECollected--;
+        }
+        else if (collision.gameObject.CompareTag("MinusEnergy"))
+        {
+            energyCount -= 5;
+            Destroy(collision.gameObject);
+        }
+    }
+}
